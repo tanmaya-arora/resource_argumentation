8e1509e36ea9090a415daad8a151949c25ffdd32
k2u4sh <122009631+k2u4sh@users.noreply.github.com>
2023-07-27 16:04:49 +0530

    ffd

---
+++ gunicorn-cfg.py
@@ -0,0 +1,12 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+wsgi_app = "core.wsgi"
+bind = '0.0.0.0'
+workers = 1
+accesslog = '-'
+loglevel = 'debug'
+capture_output = True
+enable_stdio_inheritance = True
