d39f279538d5092bf41860feac75f808f2a923b6
k2u4sh <122009631+k2u4sh@users.noreply.github.com>
2023-07-27 14:22:52 +0530

    dd

--- core/settings.py
+++ core/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # STATIC_URL = 'static/'
 
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
